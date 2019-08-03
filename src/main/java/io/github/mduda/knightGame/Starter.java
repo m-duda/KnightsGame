9f8b77e978054a24b0462c42f43d1e83d25c2deb
Michał <michal_duda@onet.pl>
2019-08-03 22:48:55 +0200

    Dependecy injection by constructor.

--- src/main/java/io/github/mduda/knightGame/Starter.java
+++ src/main/java/io/github/mduda/knightGame/Starter.java
@@ -1,6 +1,7 @@
 package io.github.mduda.knightGame;
 
 import io.github.mduda.knightGame.domain.Knight;
+import io.github.mduda.knightGame.domain.Quest;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +11,8 @@ public class Starter implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Knight lancelot = new Knight("Lancelot", 29);
+        Quest quest = new Quest("zabij smoka");
+        Knight lancelot = new Knight("Lancelot", 29, quest);
         System.out.println(lancelot.toString());
     }
 }
--- src/main/java/io/github/mduda/knightGame/domain/Knight.java
+++ src/main/java/io/github/mduda/knightGame/domain/Knight.java
@@ -3,15 +3,17 @@ package io.github.mduda.knightGame.domain;
 public class Knight {
     private String name;
     private int age;
+    private Quest quest;
 
 
-    public Knight(String name, int age) {
+    public Knight(String name, int age, Quest quest) {
         this.name = name;
         this.age = age;
+        this.quest = quest;
     }
 
     @Override
     public String toString() {
-        return "Rycerz o imieniu "+ name + "("+age+")";
+        return "Rycerz o imieniu "+ name + "("+age+") i ma za zadanie " + quest.toString() + ".";
     }
 }
