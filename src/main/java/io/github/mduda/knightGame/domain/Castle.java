a3ab433fc8acf76bbe6d9c3ef244de55b0c42ac1
Michał <michal_duda@onet.pl>
2019-08-03 22:53:00 +0200

    Create first component - Castle class

---
+++ src/main/java/io/github/mduda/knightGame/domain/Castle.java
@@ -0,0 +1,11 @@
+package io.github.mduda.knightGame.domain;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Castle {
+    private String name = "East Watch";
+
+    public Castle() {
+    }
+}
