bf354efbfe8352670ade8f0c9c1343b7f0392e95
Michał <michal_duda@onet.pl>
2019-08-03 22:58:09 +0200

    Beans life cycle - preDestroy and postConstruct annotation

--- src/main/java/io/github/mduda/knightGame/domain/Castle.java
+++ src/main/java/io/github/mduda/knightGame/domain/Castle.java
@@ -2,10 +2,24 @@ package io.github.mduda.knightGame.domain;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class Castle {
-    private String name = "East Watch";
+    private String name = "Wschodnia strażnica";
 
     public Castle() {
     }
+
+    @PostConstruct
+    public void build (){
+        System.out.println("Wybudowano zamek "+ name + ".");
+    }
+
+    @PreDestroy
+    public void destroy (){
+        System.out.println("Wyburzono zamek " + name + ".");
+    }
+
 }
