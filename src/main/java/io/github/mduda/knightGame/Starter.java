2af70d1fa28060aadcf8b87c2838ffad296f52cf
Michał <michal_duda@onet.pl>
2019-08-08 21:54:30 +0200

    Autowired annotation first usecase

--- src/main/java/io/github/mduda/knightGame/Starter.java
+++ src/main/java/io/github/mduda/knightGame/Starter.java
@@ -1,18 +1,21 @@
 package io.github.mduda.knightGame;
 
+import io.github.mduda.knightGame.domain.Castle;
 import io.github.mduda.knightGame.domain.Knight;
 import io.github.mduda.knightGame.domain.Quest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Starter implements CommandLineRunner {
 
+    @Autowired
+    Castle castle;
 
     @Override
     public void run(String... args) throws Exception {
-        Quest quest = new Quest("zabij smoka");
-        Knight lancelot = new Knight("Lancelot", 29, quest);
-        System.out.println(lancelot.toString());
+        System.out.println(castle.toString());
+
     }
 }
