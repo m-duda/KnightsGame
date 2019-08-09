9de993429964d44fdea7a9265b7ffe43c47ac006
Michał <michal_duda@onet.pl>
2019-08-09 23:01:22 +0200

    CastleTest - add test toString method

--- src/test/java/io/github/mduda/knightGame/domain/CastleTest.java
+++ src/test/java/io/github/mduda/knightGame/domain/CastleTest.java
@@ -1,5 +1,19 @@
+package io.github.mduda.knightGame.domain;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CastleTest {
+
+    @Test
+    public void testToString() {
+        String expect = "Znajduje się tu zamek Wschodnia straznica, przebywa w nim rycerz o imieniu Lancelot(29). Ma za zadanie zabij smoka.";
+        Quest quest = new Quest();
+        Knight knight = new Knight();
+        knight.setQuest(quest);
+        Castle castle = new Castle(knight,"Wschodnia straznica");
+        assertEquals(expect, castle.toString());
+    }
 
 }
