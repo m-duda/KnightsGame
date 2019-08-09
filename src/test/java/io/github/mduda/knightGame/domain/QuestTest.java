bae6a48f6ecd2d48e9e95c185d9cd87e881938a7
Michał <michal_duda@onet.pl>
2019-08-09 22:57:44 +0200

    Quest Test - test toString method

--- src/test/java/io/github/mduda/knightGame/domain/QuestTest.java
+++ src/test/java/io/github/mduda/knightGame/domain/QuestTest.java
@@ -1,5 +1,16 @@
+package io.github.mduda.knightGame.domain;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class QuestTest {
+
+    @Test
+    public void testToString(){
+        String expect = "zabij smoka";
+        Quest quest = new Quest();
+        assertEquals(expect, quest.toString());
+    }
 
 }
