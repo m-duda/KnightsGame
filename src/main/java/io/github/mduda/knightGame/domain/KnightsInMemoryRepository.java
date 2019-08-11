0f9e07ed9f40b273b8c141367818c83cc530678a
Michał <michal_duda@onet.pl>
2019-08-11 23:38:48 +0200

    Rename Castle to KnightsInMemoryRepository

---
+++ src/main/java/io/github/mduda/knightGame/domain/KnightsInMemoryRepository.java
@@ -0,0 +1,60 @@
+package io.github.mduda.knightGame.domain;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@PropertySource("classpath:castle.properties")
+public class KnightsInMemoryRepository {
+    
+    @Value("${my.castle.name:Czarny Zamek}")
+    private String castleName;
+
+    private Map<String, Knight> knights = new HashMap<>();
+
+    public KnightsInMemoryRepository() {
+    }
+
+    KnightsInMemoryRepository(String name) {
+        this.castleName = name;
+    }
+
+    public void createKniht (String name, int age){
+        knights.put(name, new Knight(name, age));
+    }
+
+    public Knight getKnight (String name){
+        return knights.get(name);
+    }
+
+    public Collection<Knight> getAllKnights (){
+        return knights.values();
+    }
+
+    public void removeKnight (String name){
+        knights.remove(name);
+    }
+
+    public void updateKnight (String name, int age){
+        Knight knight = knights.get(name);
+        knight.setName(name);
+        knight.setAge(age);
+    }
+
+    @PostConstruct
+    public void build (){
+        createKniht("Lancelot", 29);
+        createKniht("Percival", 31);
+    }
+
+    @Override
+    public String toString() {
+        return "Znajduje się tu zamek "+ name +  ".";
+    }
+}
